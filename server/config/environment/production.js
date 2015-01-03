30573e9a27d64bf8a7f6081ddeb94fd104634ccb
ArturT <arturtrzop@gmail.com>
2015-01-03 14:34:31 +0100

    Revert "Set default IP for production"
    This reverts commit 3dd938bbf157125fb1edaff5c1e21451e236a525.

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -6,7 +6,7 @@ module.exports = {
   // Server IP
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
             process.env.IP ||
-            '127.0.0.1',
+            undefined,
 
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
