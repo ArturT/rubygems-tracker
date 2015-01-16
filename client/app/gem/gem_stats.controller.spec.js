0fb2c3d8578b425c1a124ee1eb2e51e1a09248a5
ArturT <arturtrzop@gmail.com>
2015-01-16 18:59:42 +0100

    Add spec for gem stats ctrl

---
+++ client/app/gem/gem_stats.controller.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Controller: GemStatsCtrl', function () {
+
+  beforeEach(module('rubygemsTrackerApp.controllers'));
+
+  var GemStatsCtrl,
+      scope;
+  var gem = { name: 'knapsack' };
+  var promiseGem = { data: gem };
+
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    GemStatsCtrl = $controller('GemStatsCtrl', {
+      $scope: scope,
+      promiseGem: promiseGem
+    });
+  }));
+
+  it('should attach a gem to the scope', function () {
+    expect(angular.equals(scope.gem, gem)).toBe(true);
+  });
+});
