dad1a525e25e065eb7649ae9520fa7916a53f0dd
ArturT <arturtrzop@gmail.com>
2015-01-16 19:21:39 +0100

    use gemName instead of newGem on scope

--- client/app/gem/gem_add.controller.js
+++ client/app/gem/gem_add.controller.js
@@ -2,18 +2,18 @@
 
 angular.module('rubygemsTrackerApp.controllers')
   .controller('GemAddCtrl', function ($scope, $http, GemService) {
-    $scope.newGem = '';
+    $scope.gemName = '';
     $scope.savedGem = false;
     $scope.hasError = false;
     $scope.clickedSubmit = false;
 
     $scope.addGem = function() {
-      if($scope.newGem === '') {
+      if($scope.gemName === '') {
         $scope.hasError = true;
         return;
       }
       $scope.clickedSubmit = true;
-      GemService.create($scope.newGem).success(function(data){
+      GemService.create($scope.gemName).success(function(data){
         $scope.savedGem = true;
       }).error(function(data) {
         $scope.hasError = true;
