c157f73292646854d34341ae28f44a1333eb5948
ArturT <arturtrzop@gmail.com>
2015-01-16 13:33:20 +0100

    Fix e2e tests

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -10,7 +10,6 @@ describe('Main View', function() {
 
   it('should include jumbotron with correct data', function() {
     expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/ruby-gem.png$/);
   });
 });
