a5fb6a455f655cd341c2d96403a495ef6bc85d95
ArturT <arturtrzop@gmail.com>
2015-01-19 00:31:12 +0100

    Rename submit button

--- e2e/gem/add.po.js
+++ e2e/gem/add.po.js
@@ -4,7 +4,7 @@ var AddGemPage = function() {
   this.heroEl = element(by.css('.hero-unit'));
   this.h1El = this.heroEl.element(by.css('h1'));
   this.gemNameField = element(by.id('gemName'));
-  this.submitButton = element(by.id('submit'));
+  this.submitButton = element(by.id('submitButton'));
 };
 
 module.exports = new AddGemPage();
