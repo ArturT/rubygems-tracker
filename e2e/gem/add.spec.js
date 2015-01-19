af4c9e176063d8163c477ec5f6399e9a24a954f0
ArturT <arturtrzop@gmail.com>
2015-01-19 02:23:22 +0100

    Cover all error cases in e2e tests when user adds gem

--- e2e/gem/add.spec.js
+++ e2e/gem/add.spec.js
@@ -20,6 +20,27 @@ describe('Add Gem View', function() {
   });
 
   describe('when failure', function () {
+    describe('when submit empty form', function () {
+      it('has missin gem name error', function () {
+        page.addGem('');
+        expect(page.alertDanger.getText()).toBe('The gem name is missing.');
+      });
+    });
+
+    describe("when gem doesn't exist on rubygems.org", function () {
+      it('has not found error from rubygems.org', function () {
+        page.addGem('fake-gem-name');
+        expect(page.alertDanger.getText()).toBe('This rubygem could not be found.');
+      });
+    });
+
+    describe('when gem name contains spaces', function () {
+      it('has unexpected error from rubygems.org', function () {
+        page.addGem('gem name with spaces');
+        expect(page.alertDanger.getText()).toBe('Unexpected error from rubygems.org, please try later or use proper gem name.');
+      });
+    });
+
     describe('when gem name already exists in our database', function () {
       it('has unique name error', function () {
         page.addGem('rails');
