becbb04db9bd651e64d6378a430ad881bc2f8105
ArturT <arturtrzop@gmail.com>
2015-01-30 19:01:44 +0100

    call ruby gems api only when we need to get gem statistic for today

--- server/cron/jobs.js
+++ server/cron/jobs.js
@@ -9,39 +9,40 @@ var RubyGemsService = require('../services/rubygems.service');
 var DateService = require('../services/date.service');
 
 var updateGemStats = function(gem) {
-  RubyGemsService.getGem({
-    gemName: gem.name,
-    onSuccess: function (data) {
-      var currentTotalDownloads = data.downloads;
+  var today = DateService.todayWithoutHours();
+  var duplicates = _.filter(gem.gemStatistics, function(gemStatistic) {
+    return Date.parse(gemStatistic.date) == today;
+  });
 
-      var today = DateService.todayWithoutHours();
-      var duplicates = _.filter(gem.gemStatistics, function(gemStatistic) {
-        return Date.parse(gemStatistic.date) == today;
-      });
+  if (duplicates.length == 0) {
+    RubyGemsService.getGem({
+      gemName: gem.name,
+      onSuccess: function (data) {
+        var currentTotalDownloads = data.downloads;
 
-      if (duplicates.length == 0) {
         var lastGemStatistic = _.last(gem.gemStatistics);
         var recentDownloads = 0;
         if (lastGemStatistic) {
           recentDownloads = currentTotalDownloads - lastGemStatistic.totalDownloads;
         }
+
         var newGemStatistic = {
           totalDownloads: currentTotalDownloads,
           recentDownloads: recentDownloads
         };
-        gem.gemStatistics.push(newGemStatistic);
-      } else {
-        console.log(gem.name + ' record already has gemStatistic for today!');
-      }
 
-      gem.totalDownloads = currentTotalDownloads;
-      gem.save(function(err) {
-        if (err) {
-          console.error("Couldn't update gem " + gem.name, err);
-        }
-      });
-    }
-  });
+        gem.gemStatistics.push(newGemStatistic);
+        gem.totalDownloads = currentTotalDownloads;
+        gem.save(function(err) {
+          if (err) {
+            console.error("Couldn't update gem statistics for " + gem.name, err);
+          }
+        });
+      }
+    });
+  } else {
+    console.log(gem.name + ' record already has gemStatistic for today!');
+  }
 };
 
 var updateStatsForAllGems = function() {
