50969832775089864b206a358f245d77f44d56cf
ArturT <arturtrzop@gmail.com>
2015-01-31 16:24:34 +0100

    Extract gem service with updateGemStats

--- server/cron/jobs.js
+++ server/cron/jobs.js
@@ -3,53 +3,13 @@
 var CronJob = require('cron').CronJob;
 var _ = require('lodash');
 var Gem = require('../api/gem/gem.model');
-var GemStatistic = require('../api/gem/gem_statistic.model');
-var GemStatisticSchema = require('../api/gem/gem_statistic.schema');
-var RubyGemsService = require('../services/rubygems.service');
-var DateService = require('../services/date.service');
-
-var updateGemStats = function(gem) {
-  var today = DateService.todayWithoutHours();
-  var duplicates = _.filter(gem.gemStatistics, function(gemStatistic) {
-    return Date.parse(gemStatistic.date) == today;
-  });
-
-  if (duplicates.length == 0) {
-    RubyGemsService.getGem({
-      gemName: gem.name,
-      onSuccess: function (data) {
-        var currentTotalDownloads = data.downloads;
-
-        var lastGemStatistic = _.last(gem.gemStatistics);
-        var recentDownloads = 0;
-        if (lastGemStatistic) {
-          recentDownloads = currentTotalDownloads - lastGemStatistic.totalDownloads;
-        }
-
-        var newGemStatistic = {
-          totalDownloads: currentTotalDownloads,
-          recentDownloads: recentDownloads
-        };
-
-        gem.gemStatistics.push(newGemStatistic);
-        gem.totalDownloads = currentTotalDownloads;
-        gem.save(function(err) {
-          if (err) {
-            console.error("Couldn't update gem statistics for " + gem.name, err);
-          }
-        });
-      }
-    });
-  } else {
-    console.log(gem.name + ' record already has gemStatistic for today!');
-  }
-};
+var GemService = require('../services/gem.service');
 
 var updateStatsForAllGems = function() {
   console.log('CronJob: updateStatsForAllGems');
   Gem.find({}, function (err, gems) {
     if (err) { return err; }
-    _(gems).forEach(updateGemStats);
+    _(gems).forEach(GemService.updateGemStats);
   });
 };
 
@@ -58,6 +18,7 @@ var updateStatsForAllGems = function() {
 
 var gemsStatisticsJob = new CronJob({
   cronTime: '0 0 * * * *',
+  //cronTime: '*/5 * * * * *',
   onTick: updateStatsForAllGems
 });
 
---
+++ server/services/gem.service.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var _ = require('lodash');
+var RubyGemsService = require('./rubygems.service');
+var DateService = require('./date.service');
+
+module.exports = {
+  updateGemStats: function(gem) {
+    var today = DateService.todayWithoutHours();
+    var duplicates = _.filter(gem.gemStatistics, function(gemStatistic) {
+      return Date.parse(gemStatistic.date) == today;
+    });
+
+    if (duplicates.length == 0) {
+      RubyGemsService.getGem({
+        gemName: gem.name,
+        onSuccess: function (data) {
+          var currentTotalDownloads = data.downloads;
+
+          var lastGemStatistic = _.last(gem.gemStatistics);
+          var recentDownloads = 0;
+          if (lastGemStatistic) {
+            recentDownloads = currentTotalDownloads - lastGemStatistic.totalDownloads;
+          }
+
+          var newGemStatistic = {
+            totalDownloads: currentTotalDownloads,
+            recentDownloads: recentDownloads
+          };
+
+          gem.gemStatistics.push(newGemStatistic);
+          gem.totalDownloads = currentTotalDownloads;
+          gem.save(function(err) {
+            if (err) {
+              console.error("Couldn't update gem statistics for " + gem.name, err);
+            }
+          });
+        }
+      });
+    } else {
+      console.log(gem.name + ' record already has gemStatistic for today!');
+    }
+  }
+};
