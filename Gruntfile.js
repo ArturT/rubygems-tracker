13a83e66c85908ca7115388990df83babd4b1728
ArturT <arturtrzop@gmail.com>
2015-02-08 17:09:06 +0100

    Fix grunt build when Chart.js has .js in dir name which leads to error

--- Gruntfile.js
+++ Gruntfile.js
@@ -232,7 +232,7 @@ module.exports = function (grunt) {
       dist: {
         files: {
           src: [
-            '<%= yeoman.dist %>/public/{,*/}*.js',
+            '<%= yeoman.dist %>/public/{,*/}*.js$',
             '<%= yeoman.dist %>/public/{,*/}*.css',
             '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/public/assets/fonts/*'
@@ -255,7 +255,7 @@ module.exports = function (grunt) {
     usemin: {
       html: ['<%= yeoman.dist %>/public/{,*/}*.html'],
       css: ['<%= yeoman.dist %>/public/{,*/}*.css'],
-      js: ['<%= yeoman.dist %>/public/{,*/}*.js'],
+      js: ['<%= yeoman.dist %>/public/{,*/}*.js$'],
       options: {
         assetsDirs: [
           '<%= yeoman.dist %>/public',
