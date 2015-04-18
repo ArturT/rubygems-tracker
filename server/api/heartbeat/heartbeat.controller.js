a4b69c7d5d66c6b622f78f85dd601626cb5b46a7
ArturT <arturtrzop@gmail.com>
2015-04-18 17:41:52 +0200

    Update gem stats during heartbeat

--- server/api/heartbeat/heartbeat.controller.js
+++ server/api/heartbeat/heartbeat.controller.js
@@ -2,5 +2,18 @@
 
 // v1
 exports.index = function(req, res) {
+  var _ = require('lodash');
+  var Gem = require('../gem/gem.model');
+  var GemService = require('../../services/gem.service');
+
+  console.log('[' + new Date() + '] updateGemStats via heartbeat');
+  Gem.find({}, function (err, gems) {
+    if (err) {
+      console.log('[' + new Date() + '] updateGemStats via heartbeat - Error:', err);
+    } else {
+      _(gems).forEach(GemService.updateGemStats);
+    }
+  });
+
   return res.json(200, ['OK']);
 };
